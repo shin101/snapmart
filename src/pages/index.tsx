75ec6e7b4bfde0eb8513caa95c140a4789c041b6
shin101 <kendras0127@gmail.com>
2023-12-19 20:19:56 -0500

    auth part one

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
 export default function Home() {
   return (
-    <form className="flex flex-col space-y-2 p-2.5">
-      <input
-        type="text"
-        required
-        placeholder="Username"
-        className="border p-1 peer border-gray-400 rounded"
-      />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">This input is invalid</span>
-      <input type="submit" value="Login" />
-    </form>
-  );
+    <div>Hiiii</div>
+  )
 }
