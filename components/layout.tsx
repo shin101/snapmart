57b8b94439c4e15ca30985d3d1a23eceff3e9571
shin101 <kendras0127@gmail.com>
2023-12-21 17:11:17 -0500

    layout part one

---
+++ components/layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { cls } from "../libs/utils";
+
+interface LayoutProps {
+  title?: string;
+  canGoBack?: boolean;
+  hasTabBar?: boolean;
+  children: React.ReactNode;
+}
+
+const Layout = ({ title, canGoBack, hasTabBar, children }: LayoutProps) => {
+  return (
+    <div>
+      <div className="bg-white w-full text-lg font-medium py-3 fixed text-gray-800 border-b top-0 justify-center flex items-center left-0">
+        {title ? <span>{title}</span> : null}
+      </div>
+      <div className={cls("pt-16", hasTabBar ? "pb-16" : "")}> {children}</div>
+      {hasTabBar ? (
+        <nav className="bg-white text-gray-800 border-t fixed bottom-0 pb-10 pt-3 flex justify-between items-center"></nav>
+      ) : null}
+    </div>
+  );
+};
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-full max-w-4xl mx-auto">
+      <Component {...pageProps} />
+    </div>
+  );
 }
