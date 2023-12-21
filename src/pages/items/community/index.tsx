511341da0f48ee934c704e795423b2966611484c
shin101 <kendras0127@gmail.com>
2023-12-20 22:17:01 -0500

    community detail

--- src/pages/items/community/index.tsx
+++ src/pages/items/community/index.tsx
@@ -13,7 +13,7 @@ const Community: NextPage = () => {
             inspires you the most?
           </div>
           <div className="mt-5 flex items-center justify-between w-full text-gray-500 font-medium">
-            <span>니꼬</span>
+            <span>John</span>
             <span>18 hours ago</span>
           </div>
           <div className="flex space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full">
@@ -32,7 +32,7 @@ const Community: NextPage = () => {
                   d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 ></path>
               </svg>
-              <span>궁금해요 1</span>
+              <span>Curious 1</span>
             </span>
             <span className="flex space-x items-center">
               <svg
@@ -49,7 +49,7 @@ const Community: NextPage = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 ></path>
               </svg>
-              <span>답변 1</span>
+              <span>Replies 1</span>
             </span>
           </div>
         </div>
