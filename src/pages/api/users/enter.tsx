ebc23fcb6b90c453ac494acdf4ed158b146c53ce
shin101 <kendras0127@gmail.com>
2023-12-28 23:07:36 -0500

    Accounts Logic

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -4,6 +4,20 @@ import withHandler from "@libs/server/withHandler";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { phone, email } = req.body;
+  const payload = phone ? { phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+
   return res.status(200).end();
 }
 
