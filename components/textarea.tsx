2c31042e17c33a689553872df50fe5a6cdacea78
shin101 <kendras0127@gmail.com>
2024-01-07 21:27:29 -0500

    Upload Form

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import Button from "./button";
+import { UseFormRegisterReturn } from "react-hook-form";
 
 interface TextAreaProps {
+  label?: string;
+  name?: string;
+  register: UseFormRegisterReturn;
   [key: string]: any;
 }
 
-const TextArea: React.FC<TextAreaProps> = ({ ...rest }) => {
+const TextArea: React.FC<TextAreaProps> = ({
+  label,
+  name,
+  register,
+  ...rest
+}) => {
   return (
     <div>
       {" "}
       <textarea
+        id={name}
+        {...register}
         className="mt-1 shadow-sm w-full focus:ring-purple-500 rounded-md border-gray-300 focus:border-purple-500"
         rows={4}
         {...rest}
       />{" "}
-      <div className="flex items-center justify-between space-x-2">
-        <Button text="Submit" />
-      </div>
+      <div className="flex items-center justify-between space-x-2"></div>
     </div>
   );
 };
--- src/pages/products/upload.tsx
+++ src/pages/products/upload.tsx
@@ -1,50 +1,72 @@
 import type { NextPage } from "next";
 import Input from "@components/input";
 import Button from "@components/button";
+import { useForm } from "react-hook-form";
+import TextArea from "@components/textarea";
+import Layout from "@components/layout";
+import useMutation from "@libs/client/useMutation";
+
+interface UploadProductForm {
+  name: string;
+  price: number;
+  description: string;
+}
 
 const Upload: NextPage = () => {
+  const { register, handleSubmit } = useForm<UploadProductForm>();
+  const [uploadProduct, { loading, data }] = useMutation("/api/products");
+  const onValid = (data: UploadProductForm) => {
+    if (loading) return;
+    uploadProduct(data);
+  };
   return (
-    <div className="px-4 space-y-5 py-10">
-      <div>
-        <label className="w-full cursor-pointer text-gray-600 hover:border-purple-500 hover:text-purple-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
-          <svg
-            className="h-12 w-12"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-          >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <input className="hidden" type="file" />
-        </label>
-      </div>
-
-      <Input name="Name" label="Name" kind="text" placeholder="0.00" />
-      <Input name="Price" label="Price" kind="price" placeholder="0.00" />
-      <div>
-        <label
-          htmlFor="description"
-          className="mb-1 block text-sm font-medium text-gray-700"
-        >
-          Description
-        </label>
-
-        <textarea
-          id="description"
-          className="mt-1 shadow-sm w-full focus:ring-purple-500 rounded-md border-gray-300 focus:border-purple-500 "
-          rows={4}
+    <Layout canGoBack title="Upload Product">
+      <form className="px-4 space-y-5 py-10" onSubmit={handleSubmit(onValid)}>
+        <div>
+          <label className="w-full cursor-pointer text-gray-600 hover:border-purple-500 hover:text-purple-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
+            <svg
+              className="h-12 w-12"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <input className="hidden" type="file" />
+          </label>
+        </div>
+        <Input
+          register={register("name", { required: true })}
+          name="Name"
+          label="Name"
+          type="text"
+          required
         />
-      </div>
-      <div className="flex items-center justify-between space-x-2">
-        <Button text="Upload Item" />
-      </div>
-    </div>
+        <Input
+          register={register("price", { required: true })}
+          required
+          name="Price"
+          label="Price"
+          kind="price"
+          type="text"
+        />
+        <TextArea
+          register={register("description", { required: true })}
+          name="description"
+          label="Description"
+          required
+        />
+        <div className="flex items-center justify-between space-x-2">
+          <Button text={loading ? "Loading.." : "Upload Item"} />
+        </div>{" "}
+      </form>
+    </Layout>
   );
 };
 export default Upload;
