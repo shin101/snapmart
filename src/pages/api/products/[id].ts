63f0090b9b1628231cbea7b8199c399bd18cc6f8
shin101 <kendras0127@gmail.com>
2024-01-18 11:37:00 -0500

    Related Products

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -20,7 +20,24 @@ async function handler(
       },
     },
   });
-  res.json({ ok: true, product });
+  const terms = product?.name
+    .split(" ")
+    .map((word) => ({ name: { contains: word } }));
+
+  const relatedProducts = await client.product.findMany({
+    where: {
+      OR: terms,
+      AND: {
+        id: {
+          not: product?.id,
+        },
+      },
+    },
+    // show max 4 related products
+    take: 4,
+  });
+
+  res.json({ ok: true, product, relatedProducts });
 }
 
 export default withApiSession(withHandler({ methods: ["GET"], handler }));
