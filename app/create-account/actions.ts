39948511c4428711e964c8afec716de318d168b6
shin101 <kendras0127@gmail.com>
2024-04-21 21:50:42 -0700

    Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,23 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string",
+        required_error: "Username is required.",
+      })
+      .min(3, "Username is too short")
+      .max(10, "Username is too long"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(({ password, confirm_password }) => password === confirm_password, {
+    message: "Passwords do not match",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -17,6 +28,6 @@ export async function createAccount(prevState: any, formData: FormData) {
   };
   const result = formSchema.safeParse(data);
   if (!result.success) {
-    return result.error.flatten(); 
+    return result.error.flatten();
   }
 }
