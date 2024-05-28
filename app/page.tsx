57bd3368cd6d1ebdce9d8aec7fa0f199d3a9ab3a
shin101 <kendras0127@gmail.com>
2024-05-28 15:31:28 -0700

    Prisma update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import "./lib/db";
 
 export default function Home() {
   return (
@@ -9,10 +10,7 @@ export default function Home() {
         <h2 className="text-2xl">Welcome to Snapmart</h2>
       </div>
       <div className="flex flex-col items-center gap-3 w-full">
-        <Link
-          href="/create-account"
-          className="primary-btn text-lg py-2.5"
-        >
+        <Link href="/create-account" className="primary-btn text-lg py-2.5">
           Get started
         </Link>
         <div className="flex gap-2">
