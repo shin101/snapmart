2c62b382869671c0e60973b70b193905873b57b6
shin101 <kendras0127@gmail.com>
2024-05-29 00:10:40 -0700

    Log Out

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,34 @@
-export default function Profile() {
-  return <h1>Profile Page </h1>;
+import { notFound, redirect } from "next/navigation";
+import db from "../lib/db";
+import getSession from "../lib/session";
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: { id: session.id },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+  const logOut = async () => {
+    "use server";
+    const session = await getSession();
+    session.destroy();
+    redirect("/");
+  };
+  return (
+    <div>
+      <h1>Welcome, {user?.username} </h1>
+      <form action={logOut}>
+        <button>Log out</button>
+      </form>
+    </div>
+  );
 }
