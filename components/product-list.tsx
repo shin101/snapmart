74dc662c2bc186662dc69a20adf3db0a31e90bc7
shin101 <kendras0127@gmail.com>
2024-06-07 16:48:49 -0700

    #10.10 Completed infinite scroll

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ListProduct from "./list-product";
 import { InitialProducts } from "@/app/(tabs)/products/page";
 import Button from "./button";
@@ -15,31 +15,52 @@ export const ProductList = ({ initialProducts }: ProductListProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
   const [isLastPage, setIsLastPage] = useState(false);
-  const loadMoreProducts = async () => {
-    setIsLoading(true);
-    const newProducts = await getMoreProducts(page + 1);
-    if (newProducts.length !== 0) {
-      setPage((prev) => prev + 1);
-      setProducts((prev) => [...prev, ...newProducts]);
-    } else {
-      setIsLastPage(true);
+  const trigger = useRef<HTMLSpanElement>(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (
+        entries: IntersectionObserverEntry[],
+        observer: IntersectionObserver
+      ) => {
+        const element = entries[0];
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page + 1);
+          if (newProducts.length !== 0) {
+            setPage((prev) => prev + 1);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+          setIsLoading(false);
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+    if (trigger.current) {
+      observer.observe(trigger.current);
     }
-    setIsLoading(false);
-  };
+    return () => {
+      observer.disconnect();
+    };
+  }, [page]);
+
   return (
     <div className="p-5 flex flex-col gap-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      {isLastPage ? null : (
-        <button
-          onClick={loadMoreProducts}
-          disabled={isLoading}
+      {!isLastPage ? (
+        <span
+          ref={trigger}
           className="text-sm font-semibold bg-gradient-to-tr from-pink-100 via-white to-purple-200 border border-purple-400 rounded-md w-fit mx-auto px-3 "
         >
           {isLoading ? "Loading..." : "Load more"}
-        </button>
-      )}
+        </span>
+      ) : null}
     </div>
   );
 };
