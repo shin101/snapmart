06bdba70fe6d29c635f9904c9b81403b68084884
shin101 <kendras0127@gmail.com>
2024-06-11 09:57:24 -0700

    Images Setup

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,12 +1,58 @@
 "use server";
 
-const uploadProduct = async (formData: FormData) => {
+import { z } from "zod";
+import fs from "fs/promises";
+import { File } from "@web-std/file";
+import db from "@/app/lib/db";
+import { getSession } from "@/app/lib/session";
+import { redirect } from "next/navigation";
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "Photo is required",
+  }),
+  title: z.string({ required_error: "Title is required" }),
+  description: z.string({ required_error: "Description is required" }),
+  price: z.coerce.number({ required_error: "Number is required" }),
+});
+
+const uploadProduct = async (_:any, formData: FormData) => {
   const data = {
     photo: formData.get("photo"),
     title: formData.get("title"),
     price: formData.get("price"),
     description: formData.get("description"),
   };
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    data.photo = `/${data.photo.name}`;
+  }
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+    }
+  }
 };
 
 export default uploadProduct;
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -5,6 +5,7 @@ import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import uploadProduct from "./actions";
+import { useFormState } from "react-dom";
 
 const AddProduct = () => {
   const [preview, setPreview] = useState("");
@@ -23,9 +24,10 @@ const AddProduct = () => {
       return { error: "Please upload an image smaller than 4MB" };
     }
   };
+  const [state, action] = useFormState(uploadProduct, null);
   return (
     <div>
-      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+      <form action={action} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
@@ -34,7 +36,10 @@ const AddProduct = () => {
           {preview == "" ? (
             <>
               <PhotoIcon className="w-20" />
-              <div className="text-neutral-400 text-sm">Add photo</div>
+              <div className="text-neutral-400 text-sm">
+                Add photo
+                {state?.fieldErrors.photo}
+              </div>
             </>
           ) : null}
         </label>
@@ -46,13 +51,26 @@ const AddProduct = () => {
           accept="image/*"
           className="hidden"
         />
-        <Input name="title" required placeholder="Title" type="text" />
-        <Input name="price" type="number" required placeholder="Price" />
+        <Input
+          name="title"
+          required
+          placeholder="Title"
+          type="text"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          type="number"
+          required
+          placeholder="Price"
+          errors={state?.fieldErrors.price}
+        />
         <Input
           name="description"
           type="text"
           required
           placeholder="Description"
+          errors={state?.fieldErrors.description}
         />
         <Button text="Submit" />
       </form>
