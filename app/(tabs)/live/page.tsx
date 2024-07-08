4c0d7cb0705ef230ca04ca9445f282276e8d201e
shin101 <kendras0127@gmail.com>
2024-07-08 15:10:36 -0700

    deployment

--- app/(tabs)/live/page.tsx
+++ app/(tabs)/live/page.tsx
@@ -4,6 +4,7 @@ import { PlusIcon } from "@heroicons/react/24/solid";
 export default function Live() {
   return (
     <>
+      Watch Past Streams :
       <Link
         href="/streams/add"
         className="bg-purple-300 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-purple-200"
