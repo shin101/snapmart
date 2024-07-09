0b8e3f5130484580a859b4d8e7bc9b498ebd9de1
shin101 <kendras0127@gmail.com>
2024-07-08 22:55:19 -0700

    updated readme and chat page

--- app/(tabs)/chat/page.tsx
+++ app/(tabs)/chat/page.tsx
@@ -1,6 +1,5 @@
 import db from "@/lib/db";
 import Link from "next/link";
-
 async function getChatMessages() {
   const chatMessages = await db.chatRoom.findMany({
     select: {
@@ -15,27 +14,25 @@ async function getChatMessages() {
       messages: true,
     },
   });
-
   return chatMessages;
 }
 async function Chat() {
   const allMsg = await getChatMessages();
-  //   console.log(">>>", allMsg);
-  console.log(allMsg.map((msg) => msg.messages));
+
   return (
     <div>
-      <div className="border rounded-md">
-        {allMsg.map((msg) => (
-          <div
-            className="m-2 p-2 border-b border-b-purple-300 last:border-b-0"
-            key={msg.id}
-          >
-            <Link href="/products">{msg.id}</Link>
+      {allMsg.length > 0 ? (
+        allMsg.map((msg) => (
+          <div className="border rounded-md" key={msg.id}>
+            <div className="m-2 p-2 border-b border-b-purple-300 last:border-b-0">
+              <Link href="/products">{msg.id}</Link>
+            </div>
           </div>
-        ))}
-      </div>
+        ))
+      ) : (
+        <div>You have no messages</div>
+      )}
     </div>
   );
 }
-
 export default Chat;
